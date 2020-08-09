8ce6ffa4d00fca33d9ee363fd78aaab3cd3f77ce
HyeongSeoku <gudtjr3437@naver.com>
2020-08-09 23:14:41 +0900

    세 수를 비교하여 최솟값 구하기 5543

---
+++ JAVA/Mcdonald.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Mcdonald {
+    public static void main(String args[])
+    {
+        int bur[] = new int[3];     //버거 종류 3
+        int drk[] = new int[2];     //음료 종류 2
+        int setM = 0;               //세트 메뉴 가격
+        int sale = 50;              //세트 메뉴시 50원 할인
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0;i<3;i++)       //반복문으로 버거 입력(3)
+        {
+            bur[i] = sc.nextInt();
+        }
+
+        for(int i=0;i<2;i++)       //반복문으로 음료 입력(2)
+        {
+            drk[i] = sc.nextInt();
+        }
+        //버거 최솟값 위해 삼항연산
+        if(bur[0]<bur[1] && bur[0]<bur[2])  
+        {
+            setM =bur[0];
+        }else if(bur[1]<bur[0] && bur[1]<bur[2])
+        {
+            setM = bur[1];
+        }else{
+            setM=bur[2];
+        }
+        //음료 최솟값 
+        if(drk[0]<drk[1]){
+            setM +=drk[0];
+        }else{
+            setM+=drk[1];
+        }
+        //세트메뉴 할인
+        setM = setM - sale ;
+
+        System.out.println(setM);
+        
+        sc.close();
+    }
+    
+}
