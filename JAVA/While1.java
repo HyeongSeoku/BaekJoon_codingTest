6c11189379b610387d6c999fdce45b264525b784
HyeongSeoku <gudtjr3437@naver.com>
2020-08-09 21:55:05 +0900

    Basic While문 10952

---
+++ JAVA/While1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class While1{
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        int first, second;
+
+        while(true)
+        {
+            first = sc.nextInt();
+            second =sc.nextInt();
+            if(first==0 && second==0)break;
+            System.out.println(first+second);
+        }
+        sc.close();
+        
+    }
+}
