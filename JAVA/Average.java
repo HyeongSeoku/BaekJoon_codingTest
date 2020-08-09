8cb189f982fad53ca6ca786ad97833782fc5b966
HyeongSeoku <gudtjr3437@naver.com>
2020-08-09 21:56:33 +0900

    10039 평균점수

---
+++ JAVA/Average.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String args[])
+    {
+        int stu [] = new int[5];
+        int avg = 0;
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<5;i++)
+        {
+            stu[i]=sc.nextInt();
+            if(stu[i]<40)
+            {
+                stu[i]=40;
+            }
+        }
+        avg =(stu[0]+stu[1]+stu[2]+stu[3]+stu[4])/5;
+        System.out.println(avg);
+
+        sc.close();
+    }
+    
+}
