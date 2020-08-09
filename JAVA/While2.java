c40080a36b14377e95e9bec35ebbb1e386972e0c
HyeongSeoku <gudtjr3437@naver.com>
2020-08-09 21:55:54 +0900

    hasNext를 사용한 While 문 (입력 값 갯수 주어지지 X)

---
+++ JAVA/While2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class While2{
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        int first, second;
+
+        while(sc.hasNextInt())      //입력 값이 없을때 까지
+        {
+            first = sc.nextInt();
+            second =sc.nextInt();
+          System.out.println(first+second);
+        }
+
+        sc.close();
+    }
+}
