711b95a30a4fca47a7d62219af2554c6281dfdf7
HyeongSeoku <gudtjr3437@naver.com>
2020-08-11 01:37:29 +0900

    세 수 비교(Arrays.sort) 10817

---
+++ JAVA/MiddleN.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MiddleN {
+    public static void main(String args[])
+    {
+        int num[] = new int[3];
+
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<3;i++)
+        {
+            num[i]=sc.nextInt();
+        }
+
+        Arrays.sort(num);
+
+        System.out.println(num[1]);
+
+        sc.close();
+    }
+    
+}
