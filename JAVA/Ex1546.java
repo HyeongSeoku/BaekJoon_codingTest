1288936f80d2d75f8825b6076431ffb0f50a92f8
HyeongSeoku <gudtjr3437@naver.com>
2020-08-16 21:03:01 +0900

    평균값. 1546

--- JAVA/Ex1546.java
+++ JAVA/Ex1546.java
@@ -1,33 +1,31 @@
 import java.util.Scanner;
-
-import java.util.Scanner;
 import java.util.Arrays;
 public class Ex1546 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         int n =sc.nextInt();
-        int score[] = new int[n];
+        double score[] = new double[n]; //평균값이 정수가 아닐수 있으니 double 사용.
+        double max = 0;                 // 동일
       
         for(int i=0;i<n;++i)
         {
             score[i]=sc.nextInt();
+            if(score[i]>max)        //Arrays.sort 사용하지 않고, for 문 한번 돌때마다 max값 비교를 통해 최대값 구하는 방식.
+            {
+                max=score[i];
+            }
+        
         }
-        sc.close();
-        Arrays.sort(score);
-        int max = 0;
-        int ave = 0;
-        int sum = 0;
-        max = score[n];
+        sc.close();  
+        double sum = 0;
         for(int i=0;i<n;++i)
         {
             score[i]=score[i]/max*100;
             sum+=score[i];
         }
-        ave=sum/n;
-        System.out.println(sum);
-        System.out.println(max);
-        System.out.println(ave);
+        System.out.println(sum/n);
+       
     }
     
 }
