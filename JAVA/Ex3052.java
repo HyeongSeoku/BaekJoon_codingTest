6a6d6373e052203f491a94fa8486669f455586c7
HyeongSeoku <gudtjr3437@naver.com>
2020-08-16 20:58:06 +0900

    delete unused value 1546

--- JAVA/Ex3052.java
+++ JAVA/Ex3052.java
@@ -5,7 +5,6 @@ public class Ex3052 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        int num[] = new int[10];
         HashSet<Integer> hash = new HashSet<Integer>();
 
         for(int i=0;i<10;++i)
