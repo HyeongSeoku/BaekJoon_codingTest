88b110c8d2dfb19cc66808b23e894482c7f12ddd
gudtjr3437 <gudtjr3437@naver.com>
2020-08-20 18:21:54 +0900

    손익분기점
    반복문 사용 없이 프로그래밍
    - 속도 향상

---
+++ JAVA/수학1/Ex1712.java
@@ -0,0 +1,22 @@
+package 수학1;
+import java.util.Scanner;
+
+public class Ex1712 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int FixedC =sc.nextInt();
+        int VarC = sc.nextInt();
+        int LapC = sc.nextInt();
+        sc.close();
+        long result =0;
+        if(LapC-VarC<=0)
+        {
+            result = -1;
+        }else{
+            result =(FixedC/(LapC-VarC))+1;
+        }
+        System.out.println(result);
+    }
+    
+}
