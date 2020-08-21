3de717a76070fdf852025e9af0c0f926f58f2514
gudtjr3437 <gudtjr3437@naver.com>
2020-08-21 20:58:43 +0900

    벌집
    6각형으로 이뤄진 방 ,
    시작 방 =1
    목적지 방 = 입력 값
    최소 이동 횟수 구하기

---
+++ JAVA/수학1/Ex2292.java
@@ -0,0 +1,30 @@
+package 수학1;
+import java.util.Scanner;
+public class Ex2292 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int result = 0;
+        sc.close();
+        result =Room(N);
+        System.out.println(result);
+    }   
+    private static int Room(int n)
+    {
+        int RoomN = 1;
+        int addN = 0;
+        int res = 0;
+        while(true)
+        {
+            if(n<=RoomN)
+            {
+                break;
+            }
+            RoomN+=addN;
+            addN+=6;
+            res++;
+        }
+        return res;
+    } 
+}
