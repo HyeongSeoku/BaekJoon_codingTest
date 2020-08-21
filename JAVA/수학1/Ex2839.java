cdfd4a5842adcbeb230fbfe6537887371b96cb38
gudtjr3437 <gudtjr3437@naver.com>
2020-08-21 19:48:45 +0900

    설탕배달
    5kg 3kg 설탕봉지로 무게(입력값)를 맞추어 배달.
    단, 최소한의 봉지로 배달

---
+++ JAVA/수학1/Ex2839.java
@@ -0,0 +1,40 @@
+package 수학1;
+import java.util.Scanner;
+
+public class Ex2839 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        while(N>0)
+        {
+             if(N%5==0)
+             {
+                 System.out.println(N/5);
+                 return ;
+             }
+             else
+             {
+                int share = N/5;
+                for(int i=share;i>0;--i)
+                {
+                    int tmp = N-(i*5);
+                    if(tmp%3==0)
+                    {
+                        System.out.println(i+(tmp/3));
+                        return;
+                    }
+                }
+
+             }
+             if(N%3==0)
+             {
+                 System.out.println(N/3);
+             }else{
+                 System.out.println(-1);
+             }
+             return;
+        }
+        
+    }    
+}
