db6b0fd162f6adf632294404f49de1343e5f1a98
gudtjr3437 <gudtjr3437@naver.com>
2020-08-22 16:23:21 +0900

    피보나치 수열

---
+++ JAVA/재귀/Ex10870.java
@@ -0,0 +1,25 @@
+package 재귀;
+import java.util.Scanner;
+
+public class Ex10870 {
+    public static void main(String args[])
+    {   
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int result = 0;
+        sc.close();
+
+        result=+fibonacci(N);   //Fibonacci함수 리턴값 결과에 저장
+
+        System.out.println(result);
+        
+    }
+    private static int fibonacci(int n)
+    {
+        if(n<=1)
+            return n;
+        else
+            return fibonacci(n-2)+fibonacci(n-1);
+    }
+    
+}
