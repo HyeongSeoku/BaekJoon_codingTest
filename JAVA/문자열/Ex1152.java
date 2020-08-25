2f56b212c1143ae4cb8f592e1ffb611fea0daab4
gudtjr3437 <gudtjr3437@naver.com>
2020-08-25 16:04:59 +0900

    단어의 개수
    오답 이유 : .nextLine() 사용하지 않았기 때문

--- JAVA/문자열/Ex1152.java
+++ JAVA/문자열/Ex1152.java
@@ -5,17 +5,24 @@ public class Ex1152 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        String sentence = sc.next();        //입력 받을 문자
+        String sentence = sc.nextLine();        //입력 받을 문자
+        sc.close();
         int count = 0;                      //공백을 셀 카운트 함수
-        
+        sentence = sentence.trim();//앞뒤 빈칸 제거
+
         for(int i=0;i<sentence.length();++i)
         {
-            if(sentence.charAt(i)==' ')     //입력받은 문자열을 하나하나 체크 , 공백일시 카운트 증가
+            if(sentence.charAt(i)==' ') //입력받은 문자열을 하나하나 체크 , 공백일시 카운트 증가
             {
                 count++;
             }
         }
-        System.out.println(count+1);
+        if(sentence.length()==0)    //공백만 입력될 경우
+        {
+            System.out.println(0);
+        }else
+            System.out.println(count+1);
+      
     }
     
 }
