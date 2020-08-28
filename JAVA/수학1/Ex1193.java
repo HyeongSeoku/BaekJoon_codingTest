3c8d6a009a9431873f7bd154758368eed7e581ae
gudtjr3437 <gudtjr3437@naver.com>
2020-08-28 18:00:49 +0900

    분수찾기(Wrong answer)
    이전 대각선 수의 합 과 대각선 수
    두가지 변수를 이용
    
    짝수일때 와 홀수일때로 나눠서 출력

---
+++ JAVA/수학1/Ex1193.java
@@ -0,0 +1,44 @@
+package 수학1;
+import java.util.Scanner;
+public class Ex1193 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        //=====입력문========
+        int cross_cnt = 1 ,pre_cnt_sum =0;
+      while(true)
+      {
+       
+        //대각선 수 + 이전 대각선 수가 입력값보다 크거나 같을때
+        if(N <= cross_cnt+pre_cnt_sum)
+        {   
+            
+            //짝수일때
+            if(N%2== 0)
+            {                                                //  ↓ 0이됨 
+                //분자: N-이전 대각선 합 , 분모 : 대각선 수 -(N - 이전대각선 합 -1)
+                System.out.print((N-pre_cnt_sum)+ "/" +(cross_cnt-(N-pre_cnt_sum-1)));
+                break;
+            }
+            
+            //홀수일때
+            else
+            {   
+                //짝수일때 와 반대로 출력
+                System.out.print((cross_cnt-(N-pre_cnt_sum-1))+ "/" +(N-pre_cnt_sum));
+                break;
+            }
+        }
+        else
+        {
+            pre_cnt_sum += cross_cnt;
+            cross_cnt++;
+        }
+    
+       }  
+       
+    }
+    
+}
