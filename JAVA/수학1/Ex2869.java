74d59763e790a0d542ec3de98cd9c8e87ecac525
gudtjr3437 <gudtjr3437@naver.com>
2020-08-29 16:55:02 +0900

    달팽이는 오르고 싶다
    핵심 : int day = (V - down) / (up - down)

--- JAVA/수학1/Ex2869.java
+++ JAVA/수학1/Ex2869.java
@@ -2,32 +2,22 @@ package 수학1;
 import java.util.Scanner;
 
 public class Ex2869 {
-    public static void main(String args[])
-    {
-        //높이 V 달팽이 A미터 낮 올라감 밤에 B미터 미끄러짐
-        // 정상에 오른후는 미끄러지지 않음
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B =sc.nextInt();
-        int V =sc.nextInt();
+ 
+	public static void main(String args[]) {
+ 
+		Scanner sc = new Scanner(System.in);
+ 
+		int up = sc.nextInt();		// A
+		int down = sc.nextInt();	// B
+		int V = sc.nextInt(); 	// C
         sc.close();
-        //===========입력문==========
-        int count = 0;  //걸리는 날짜
-        int sum =0 ;  
-        while(true)
-        {
-            //높이에 도달할 경우
-            if(sum==V)
-            {
-                System.out.println(count);
-                break;
-            }
-            //
-            else{
-                count++;
-                sum = count*(A-B);
-            }
-        }
 
-    }
+		int day = (V - down) / (up - down);
+        
+		// 나머지가 있을 경우 (잔여 블럭이 있을 경우)
+		if ((V - down) % (up - down) != 0) {
+			day++;
+		}
+		System.out.println(day);
+	}
 }
