2f2f23d14c1b81107732cc3cac441f35e8419067
gudtjr3437 <gudtjr3437@naver.com>
2020-08-30 16:40:19 +0900

    ACM 호텔
    YXX = 방번호
    Y = N%H
    X = N/H+1
    단 , N%H==0 일 경우에만 예외 발생
    (조건문)

---
+++ JAVA/수학1/Ex10250.java
@@ -0,0 +1,32 @@
+package 수학1;
+import java.util.Scanner;
+public class Ex10250 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();   //테스트 케이스 개수
+        
+        for(int i=0;i<T;++i)
+        {
+            int H = sc.nextInt();   //층수 
+            int W = sc.nextInt();   //방수
+            int N = sc.nextInt();   //몇번째 손님
+
+            //========입력문=============
+            sc.close();
+
+            
+            //N%H == 0 일경우에만 예외가 발생.
+            if(N%H == 0)
+            {
+                System.out.println(H*100+(N/H));
+                
+            }
+            else
+            {
+                System.out.println((N%H)*100 + (N/H+1));
+            }
+            
+        }
+        
+    }
