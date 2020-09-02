9123ba3b85dc51b1c2685ee9cf6588b5ba876c95
gudtjr3437 <gudtjr3437@naver.com>
2020-09-02 16:53:48 +0900

    소수 구하기
    에라토스테네스의 체 이용
    (get_prime 메소드)

--- JAVA/수학2/Ex1929.java
+++ JAVA/수학2/Ex1929.java
@@ -1,29 +1,36 @@
 package 수학2;
 import java.util.Scanner;
 public class Ex1929 {
+    public static boolean[] prime;
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
 
-        int M =sc.nextInt();
-        int N =sc.nextInt();
-        int arr [] = new int [N+1];
-        for(int i=M; i<=N; ++i)
+        int M = sc.nextInt() , N = sc.nextInt();
+
+        prime = new boolean[N+1];
+        get_prime();
+
+        for(int i=M; i<=N;++i)
         {
-            arr[i]=i;
-            int sosu = 0;
-            for(int j=1; j<=i; ++j)
-            {
-                if(arr[i] % j ==0)
-                {
-                    sosu++;
-                }
-            }
-                if(sosu == 2)
-                {
-                    System.out.println(arr[i]);
-                }
+            if(!prime[i])
+            System.out.println(i);
         }
 
+        
+    }
+    public static void get_prime(){
+        //true = 소수 아님 , false = 소수
+         
+        prime[0] = prime[1] = true;
+        for(int i=2; i<=Math.sqrt(prime.length); ++i)
+        {
+            if(prime[i]) continue;
+            for(int j=i*i; j<prime.length; j+=i)
+            {
+                //배수들은 소수가 아님.
+                prime[j] = true;
+            }
+        }
     }
 }
