326d5e6ca50d3d01531f0044f22a24693db2e65c
gudtjr3437 <gudtjr3437@naver.com>
2020-09-10 18:31:42 +0900

    직사각형에서 탈출
    Math.min(a,b) 메소드 사용

---
+++ JAVA/수학2/Ex1085.java
@@ -0,0 +1,16 @@
+package 수학2;
+import java.util.Scanner;
+
+public class Ex1085 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt() , Y = sc.nextInt() , W =sc.nextInt() , H =sc.nextInt();
+
+        int resultY = Math.min(Y,H-Y);      //Y 와 H-Y 중 최소값
+        int resultX = Math.min(X,W-X);
+
+        System.out.println(Math.min(resultY,resultX));  
+    }
+}
