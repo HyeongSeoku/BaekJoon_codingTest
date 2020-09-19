2be3110567075862e47ebeaae4058319868bb464
gudtjr3437 <gudtjr3437@naver.com>
2020-09-19 21:10:03 +0900

    Create Ex11729.java

---
+++ JAVA/재귀/Ex11729.java
@@ -0,0 +1,12 @@
+package 재귀;
+import java.util.Scanner;
+
+public class Ex11729{
+    public static void main(String args[])
+    {
+
+    }
+    public static int Hanoi(int n){
+        return n;
+    }
+}
