e263d17ef3b6529fc0caac7d5a2c1bb57a6b3699
gudtjr3437 <gudtjr3437@naver.com>
2020-10-02 23:21:59 +0900

    숫자카드 2 (Error)

--- 클래스2/Ex10816.java
+++ 클래스2/Ex10816.java
@@ -20,13 +20,55 @@ public class Ex10816 {
         }
         Arrays.sort(card);
         Arrays.sort(num);
+        for(int z = 0; z<num.length; ++z)
+        {
+            int result = 0;
+            result = Binary(card, num[z]);
+            System.out.println(result+"");
+        }
+
 
     }
-    public static int Binary(int n[], int m[])
+    public static int Binary(int n[], int key)
     {
-        for(int i=0;i<N; ++i)
+        int left =0 , middle , right = n.length -1 ;
+        int cnt = 0;
+        for(int i=0;i<n.length; ++i)
         {
-            
+                
+           while(left<=right)
+           {
+               middle =(left + right)/2;
+
+               if(key ==n[middle])
+               {
+                   cnt++;
+                   if(n[middle-1]== key)
+                   {
+                       middle -=1 ;
+                       while(middle-->0)
+                       {
+                           cnt ++;
+                           if(n[middle]!=key)
+                           {
+                               break;
+                           }
+                       }
+                   }
+               }    //찾는값이 중앙값과 같을경우
+               else if(key<middle)
+               {
+                 right = middle -1 ;
+                 middle = left+right /2 ;
+               }    //찾는 값이 중앙값보다 작을경우
+               else if(key>middle)
+               {
+                 left = middle +1 ;
+                 middle = left+right /2;
+               }    //찾는 값이 중앙값보다 클경우
+               
+           } 
         }
+        return cnt;
     }
 }
