9b9da2e61ef87f7717102f07888e3d19d5eaab91
gudtjr3437 <gudtjr3437@naver.com>
2020-12-22 15:20:32 +0900

    프린터 큐

--- 클래스2/Ex1966.java
+++ 클래스2/Ex1966.java
@@ -1,55 +1,45 @@
 package 클래스2;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.Queue;
 import java.util.LinkedList;
 public class Ex1966 {
     public static void main(String args[]) throws IOException {
         Scanner sc = new Scanner(System.in);
         int Case = sc.nextInt();  //테스트 케이스 개수
-        LinkedList <Integer> q = new LinkedList<>();
-        StringBuilder st = new StringBuilder();
+        int N,M,count ;
+        LinkedList <int[]> queue = new LinkedList<>();
         for(int i=0;i<Case; ++i)
         {
-            int N = sc.nextInt();   //문서의 갯수
-            int M = sc.nextInt();   //조회를 원하는 문서의 위치(번호)
- 
-            int count = 0;
-            int Imp[] = new int[N];    //중요도
-            q.clear();
+             N = sc.nextInt();   //문서의 갯수
+             M = sc.nextInt();   //조회를 원하는 문서의 위치(번호)
+             count = 0;
+            queue.clear();
             for(int j=0; j<N; ++j)
             {
-                q.add(Imp[i]=sc.nextInt());
+                queue.add(new int[] {j,sc.nextInt()});
             }
 
-            while(!q.isEmpty())
+            while(!queue.isEmpty())
             {
+                int now [] = queue.poll();
                 boolean isPriority = true;
 
-                for(int z=0; z<q.size(); ++z)
-                {
-                    if(q.peek()>q.get(z))
+                for(int[] q : queue)
+                    if(q[1]> now[1])
                     {
                         isPriority = false;
-                        break;
                     }
-                }
                 if(isPriority){
                     count++;
-                    q.poll();
-
-                    if(M==0) break;
-                    else M-=1;
+                    if(now[0]==M) break;
                 }
                 else{
-                    int temp = q.poll();
-                    q.add(temp);
-                    M=(M==0)? q.size()-1 : --M ;
+                   queue.add(now);
                 }
             }
-            st.append(count).append("\n");         
+            System.out.println(count);
         }    
-            System.out.println(st);
+            
     }
 }
-//실패 2020.10.08
+//성공 2020.12.22
