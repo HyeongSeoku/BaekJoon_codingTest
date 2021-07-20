25e18f9b0283ab7a835c854a1d30efc06c1c39be
gudtjr3437 <gudtjr3437@naver.com>
2021-07-20 14:56:25 +0900

    My First Python!!

--- 클래스2/Ex10845.java
+++ 클래스2/Ex10845.java
@@ -1,83 +1,83 @@
 package 클래스2;
+
 import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.Queue;
+
 public class Ex10845 {
-    public static void main(String args[]) throws Exception
-    {
-        Scanner sc =new Scanner(System.in);
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         sc.nextLine();
-        StringBuilder st = new StringBuilder(); //결과 출력할 문자열 담을 빌더
+        StringBuilder st = new StringBuilder(); // 결과 출력할 문자열 담을 빌더
         Queue<Integer> q = new LinkedList<Integer>();
-        int x ;
-        int last =0 ;
+        int x;
+        int last = 0;
         q.clear();
-        for(int i=0;i<N; ++i)
-        {
+        for (int i = 0; i < N; ++i) {
             String str = sc.nextLine();
 
-            String s[] = str.split(" "); //공백으로 문자열 구분
+            String s[] = str.split(" "); // 공백으로 문자열 구분
             String cmd = s[0];
-           
-            if(cmd.equals("push")){
+
+            if (cmd.equals("push")) {
                 x = Integer.parseInt(s[1]);
                 last = x;
-                q.offer(x); 
-            } //push 문
+                q.offer(x);
+            } // push 문
 
-            else if(cmd.equals("pop")){
-                if(q.isEmpty()){
+            else if (cmd.equals("pop")) {
+                if (q.isEmpty()) {
                     st.append("-1");
                     st.append("\n");
-                }else{
+                } else {
                     st.append(q.poll());
                     st.append("\n");
                 }
-            }  //pop 문
-            
-            else if(cmd.equals("size")){
-                if(q.isEmpty()){
+            } // pop 문
+
+            else if (cmd.equals("size")) {
+                if (q.isEmpty()) {
                     st.append("0");
                     st.append("\n");
-                }else{
+                } else {
                     st.append(q.size());
                     st.append("\n");
                 }
-            }  //size 문
+            } // size 문
 
-            else if(cmd.equals("empty")){
-                if(q.isEmpty()){
+            else if (cmd.equals("empty")) {
+                if (q.isEmpty()) {
                     st.append("1");
                     st.append("\n");
-                }else{
+                } else {
                     st.append("0");
                     st.append("\n");
                 }
-            }  //empty 문
+            } // empty 문
 
-            else if(cmd.equals("front")){
-                if(q.isEmpty()){
+            else if (cmd.equals("front")) {
+                if (q.isEmpty()) {
                     st.append("-1");
                     st.append("\n");
-                }else{
+                } else {
                     st.append(q.peek());
                     st.append("\n");
                 }
-            }  //front 문
+            } // front 문
 
-            else if(cmd.equals("back")){
-                if(q.isEmpty()){
+            else if (cmd.equals("back")) {
+                if (q.isEmpty()) {
                     st.append("-1");
                     st.append("\n");
-                }else{
+                } else {
                     st.append(last);
                     st.append("\n");
                 }
-            }  //back 문
+            } // back 문
 
-        }  //for문
-        
+        } // for문
+
         System.out.println(st);
     }
 }
