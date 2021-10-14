87313d348f196286c79db12381cc93be04310f70
gudtjr3437 <gudtjr3437@naver.com>
2021-10-14 23:36:34 +0900

    기찍 N

---
+++ python/2742.py
@@ -0,0 +1,14 @@
+'''
+    문제 이름 : 기찍 N
+    문제 번호 : 2742
+    문제 링크 : https://www.acmicpc.net/problem/2742
+'''
+
+N = int(input())
+
+for i in range(N, 0, -1):
+    print(i)
+'''
+    reversed()를 사용한 방식
+    for i in reversed(range(N))
+'''
