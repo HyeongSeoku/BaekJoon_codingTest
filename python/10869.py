3e9a92ca2049681c582ad6a74c6b823d15b95b4c
gudtjr3437 <gudtjr3437@naver.com>
2021-10-17 23:17:05 +0900

    [10869] 사칙 연산

---
+++ python/10869.py
@@ -0,0 +1,13 @@
+'''
+    문제 이름 : 사칙연산
+    문제 번호 : 10869
+    문제 링크 : https://www.acmicpc.net/problem/10869
+'''
+
+a, b = map(int, input().split())
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b))
+print(a % b)
