0401e4086214fd7d1a8b9130a6e073579de0bf7b
gudtjr3437 <gudtjr3437@naver.com>
2021-10-17 23:12:04 +0900

    [10818] 최소,최대

---
+++ python/10818.py
@@ -0,0 +1,12 @@
+'''
+    문제 이름 : 최소,최대
+    문제 번호 : 10818
+    문제 링크 : https://www.acmicpc.net/problem/10818
+'''
+
+N = int(input())
+nums = list(map(int, input().split()))
+
+print(min(nums), max(nums))
+
+# for문 쓸 필요 없음
