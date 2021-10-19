9c5001a8382462d515d2b6f4e40a1923eb78e3ca
gudtjr3437 <gudtjr3437@naver.com>
2021-10-19 23:04:18 +0900

    [1158] 요세푸스 문제

--- python/silver/1158.py
+++ python/silver/1158.py
@@ -4,24 +4,21 @@
     문제 링크 : https://www.acmicpc.net/problem/1158
 '''
 import sys
-from queue import Queue
+from collections import deque
 
-n, k = map(int, sys.stdin.readline().split())
-q = Queue()
+n, k = map(int, sys.stdin.readline().split())  # 입력 받음
 
-result = []
+circle = deque([i for i in range(1, n+1)])  # deque 사용 사람들 넣어줌
 
-for i in range(1, n+1):
-    q.put(i)
+result = []  # 빼준 사람들 담을 리스트
 
-    # 모든 사람이 빠질때까지
-while not q.empty():
-    # 큐에서 i번째 사람을 빼고 넣고 반복
-    for i in range(k):
-        num = q.get()
+print("<", end="")
+for i in range(n):
+    circle.rotate(1-k)
 
-        if i == k-1:  # 찾으려는 사람 발견시
-            result.append(num)
-        else:
-            q.put(num)  # 찾으려는 사람 아닐경우 다시 넣기
-print(result)
+    if i != n-1:
+        print(circle.popleft(), end=', ')
+    else:  # 마지막 원소일시 , 없이 출력
+        print(circle.popleft(), end="")
+
+print(">")
