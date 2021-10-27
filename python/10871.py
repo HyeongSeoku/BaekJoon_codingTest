8b668db4366df10e69d5cce28e9d4050ba230565
gudtjr3437 <gudtjr3437@naver.com>
2021-10-27 20:56:27 +0900

    [10871] X보다 작은 수

---
+++ python/10871.py
@@ -0,0 +1,16 @@
+'''
+    문제 이름 : X보다 작은 수
+    문제 번호 : 10871
+    문제 링크 : https://www.acmicpc.net/problem/10871
+'''
+
+import sys
+
+read = sys.stdin.readline
+N, X = map(int, read().split())
+
+nums = list(map(int, read().split()))
+
+for num in nums:
+    if num > X:
+        print(num, end=" ")
