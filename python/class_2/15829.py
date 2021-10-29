fcdda2ba1bf6ceaa8a46389aab904df831bd5fe5
gudtjr3437 <gudtjr3437@naver.com>
2021-10-29 17:56:16 +0900

    [15829] Hashing

--- python/class_2/15829.py
+++ python/class_2/15829.py
@@ -3,17 +3,15 @@
     문제 번호 : 15829
     문제 링크 : https://www.acmicpc.net/problem/15829
 '''
-import sys;
-read = sys.stdin.readline;
+import sys
+read = sys.stdin.readline
 
-L = int(read());
-string = read();
-answer = 0;
+L = int(read())
+string = read()
+answer = 0
 
-#모두 소문자로 구성되어 있으므로 -96
+# 모두 소문자로 구성되어 있으므로 -96
 for i in range(L):
-    answer += ((ord(string[i]) -96)*(31**i));
+    answer += ((ord(string[i]) - 96)*(31**i))
 
-print(answer % 1234567891);
-
-
+print(answer % 1234567891)
