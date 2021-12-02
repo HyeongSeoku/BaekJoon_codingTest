9212073691e3d20fc5754bd91aac218ada50931a
HyeongSeoku <gudtjr3437@naver.com>
2021-12-02 10:58:22 +0900

    [18111] 마인크래프트 (시간초과)

--- python/class_2/18111.py
+++ python/class_2/18111.py
@@ -3,36 +3,36 @@
     문제 번호 : 18111
     문제 링크 : https://www.acmicpc.net/problem/18111
 '''
+#완전 탐색 문제
 
 import sys;
 read = sys.stdin.readline;
 
-n,m,b = map(int,read().split());
-#리스트에 삽입 n번 반복
+n,m,b = map(int,read().split())
 table = [list(map(int,read().split())) for _ in range(n)]
-time,height = 9223372036854775807,0     #시간을 이렇게 설정해주는 이유는 ? 
-#모든 높이를 돌면서
+time,height = 999999999999,0      
+
 for curH in range(257):
-    bot = top = 0       #높이 낮을때를 담을 bot , 높이가 높을때를 담을 top
+    botSum = topSum = 0    #높이 낮을때를 담을 bot , 높이가 높을때를 담을 top
     for i in range(n):
         for j in range(m):
             # 높이보다 낮으면
             if table[i][j] < curH:
-                bot += curH - table[i][j]      #bot = 현재높이 - 테이블에 입력된 높이
-            # 높이보다 높을 경우
+                botSum += curH - table[i][j]
+            #높이가 높을때
             else:
-                top += table[i][j] - curH   # top = 테이블 높이 - 현재 높이
-    
-    if bot > top + b:
+                topSum += table[i][j] - curH
+
+    #range n의 범위를 나와서 확인해야함 !
+    inventory = topSum +b
+    if botSum > inventory:
+        #curH일 때의 botSum이 topSum + 인벤토리의 갯수보다 클 경우 
         continue;
-    t = bot + top *2
-    #걸린 시간이 이전에 저장된 시간보다 작거나 같을경우
+    #new 걸린 시간
+    t = botSum + topSum*2
+    #기존의 걸린 시간보다 새로운 걸린시간이 더 작거나 같을경우
     if t <= time:
-        time = t        #재할당
-        height = curH   #높이 재할당
+        time = t    #시간 재할당
+        height = curH   #높이 재할당 (curH가 0부터 257까지 증가하므로 높이가 높은 갚으로 바뀜)
+
 print(time,height)
-
-
-'''
-    완전 탐색 문제
-'''
