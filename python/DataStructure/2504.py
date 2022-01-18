01e862688fabbaf10158fe562676dab4ca68112e
HyeongSeoku <gudtjr3437@naver.com>
2022-01-18 12:29:47 +0900

    [2504] 괄호의 값

--- python/DataStructure/2504.py
+++ python/DataStructure/2504.py
@@ -4,6 +4,51 @@
     문제 링크 : https://www.acmicpc.net/problem/2504
 '''
 
+import sys
+read = sys.stdin.readline
+
+brackets = list(read().rstrip())  # 입력받은 괄호들
+
+stack = []
+tmp = 1
+answer = 0
+
+for i in range(len(brackets)):
+    # 열린 괄호 ( 일때
+    if brackets[i] == "(":
+        stack.append(brackets[i])
+        tmp *= 2
+    # 열린 괄호 [ 일때
+    elif brackets[i] == "[":
+        stack.append(brackets[i])
+        tmp *= 3
+    # 닫힌 괄호 ) 일때
+    elif brackets[i] == ")":
+        # 스택이 비어있거나 , 스택의 마지막 원소가 ( 아닐경우
+        if not stack or stack[-1] != "(":
+            answer = 0  # 정답 값 0으로 초기화
+            break
+        # 바로 직전이 ( 일 경우 즉 , 추가적으로 곱할 필요가 없는 경우
+        if brackets[i - 1] == "(":
+            answer += tmp
+        stack.pop()
+        tmp //= 2
+    else:
+        # 스택이 비어있거나 , 스택의 마지막 원소가 ( 아닐경우
+        if not stack or stack[-1] != "[":
+            answer = 0  # 정답 값 0으로 초기화
+            break
+        if brackets[i-1] == "[":
+            answer += tmp
+        stack.pop()
+        tmp //= 3
+
+if stack:
+    print(0)
+else:
+    print(answer)
+
+
 '''
 열린 괄호 ( , [ 이면 일단 스택에 넣음
 if 닫힌 괄호
@@ -18,45 +63,3 @@ if 닫힌 괄호
 닫힌 괄호 이후에 바로 열린 괄호일 경우 = result + tmpSum
 
 '''
-
-import sys
-read = sys.stdin.readline
-
-brackets = list(read().rstrip())  # 입력받은 괄호들
-
-stack = []
-tmpSum = 1  # 열린 괄호들을 계산할 중간 tmpSum
-result = 0  # 총합이 담길 result
-
-for i in range(len(brackets)):
-    if brackets[i] == "(" or brackets[i] == "[":
-        if brackets[i-1] == ')' or brackets[i-1] == "]":
-            stack.append(brackets[i])
-            result += tmpSum
-            tmpSum = 1
-        # 열린 괄호일 경우 스택에 append
-        else:
-            stack.append(brackets[i])
-    else:
-        # 닫힌 괄호일 경우
-        if brackets[i] == ")" and stack[-1] == "(":
-            # )이고 스택의 마지막이 ( 일경우
-            tmpSum *= 2
-            stack.pop()
-
-        elif brackets[i] == "]" and stack[-1] == "[":
-            # ]이고 스택의 마지막이 [ 일경우
-            tmpSum *= 3
-            stack.pop()
-        else:
-            # 짝이 안 맞는 경우
-            print(0)
-            exit(0)
-    if len(stack) == 0:
-        result += tmpSum
-        tmpSum = 1
-
-
-print(result)
-
-# 런타임 오류가 뜨는 이유는 ??
