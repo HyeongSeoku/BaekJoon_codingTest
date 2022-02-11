e4fb5f78f37a3be879181cf6c976425706e07c0e
HyeongSeoku <gudtjr3437@naver.com>
2022-02-11 12:28:00 +0900

    [1002] : A / B

---
+++ js/class_1/1002.js
@@ -0,0 +1,5 @@
+//백준에 업로드할시 readFileSync("/dev/stdin")
+const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+console.log(a / b);
