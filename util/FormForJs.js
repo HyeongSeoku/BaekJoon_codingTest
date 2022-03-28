307dd014377a121308db0fcde3b55c68a4990447
HyeongSeoku <gudtjr3437@naver.com>
2022-03-28 12:27:07 +0900

    update : update template file

--- util/FormForJs.js
+++ util/FormForJs.js
@@ -3,3 +3,13 @@
     문제 번호 : 
     문제 링크 : 
 */
+
+const fs = require("fs");
+
+// 백준 제출시 주석 제거
+// const readFileSyncAddress = "/dev/stdin"
+
+// VScode 테스트시 주석 제거
+const readFileSyncAddress = "example.txt";
+
+const input = fs.readFileSync(readFileSyncAddress).toString().split("\n");
