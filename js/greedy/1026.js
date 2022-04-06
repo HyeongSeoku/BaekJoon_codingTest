f708814bdae6ced6fbb2b90282a13869471f558d
HyeongSeoku <gudtjr3437@naver.com>
2022-04-06 12:09:26 +0900

    [1026] : 보물

---
+++ js/greedy/1026.js
@@ -0,0 +1,36 @@
+/*
+    문제 이름 : 보물
+    문제 번호 : 1026
+    문제 링크 : https://www.acmicpc.net/problem/1026
+*/
+
+const fs = require("fs");
+
+// 백준 제출시 주석 제거
+// const readFileSyncAddress = "/dev/stdin"
+
+// VScode 테스트시 주석 제거
+const readFileSyncAddress = "example.txt";
+
+const input = fs
+  .readFileSync(readFileSyncAddress)
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, ...arr] = input;
+
+const [A, B] = arr.map((i) => i.split(" ").map((i) => +i));
+
+solution(N, A, B);
+
+function solution(n, listA, listB) {
+  let answer = 0;
+  listA.sort((a, b) => b - a);
+  listB.sort((a, b) => a - b);
+
+  for (let i = 0; i < n; i++) {
+    answer += listA[i] * listB[i];
+  }
+  console.log(answer);
+}
