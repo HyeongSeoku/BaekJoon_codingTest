4bca58ceecb50be3ebb16d891eeb79064656dbb8
김형석 <gudtjr3437@jobkorea.co.kr>
2023-01-01 16:47:26 +0900

    Modify : Delete Class 1

--- js/class_1/1000.js
+++
@@ -1,5 +0,0 @@
-//백준에 업로드할시 readFileSync("/dev/stdin")
-const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
-const a = parseInt(input[0]);
-const b = parseInt(input[1]);
-console.log(a + b);
